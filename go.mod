ce12d6a07ce6ccea2898c07bd0d7419d1c515a3b
Aarushi Singhal <Aarushi.Singhal@watchguard.com>
2024-07-25 16:23:48 +0530

    Adding changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/asinghal20/utility
+module github.com/asinghal20/utility/v2
 
 go 1.22.5
--- testImport/testImport.go
+++ testImport/testImport.go
@@ -7,3 +7,7 @@ var ToBeUsed string = "I am the imported variable"
 func ToBeUsedFn() {
 	fmt.Println(ToBeUsed)
 }
+
+func NewImportedFn() {
+	fmt.Println("I am new")
+}
